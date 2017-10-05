7e2cb20802228c161f4ac9d7672c27bc9cc2cb7c
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-10-05 15:38:43 -0700

    dont initialize assets on precompile

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module VideoChatApp
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.initialize_on_precompile = false
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
