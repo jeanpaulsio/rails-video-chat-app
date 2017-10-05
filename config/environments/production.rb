7277ac6b72851830c9d6fb35c4e658f37fe8c1b7
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-10-05 15:31:30 -0700

    configure heroku for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -41,11 +41,11 @@ Rails.application.configure do
 
   # Mount Action Cable outside main process or domain
   # config.action_cable.mount_path = nil
-  # config.action_cable.url = 'wss://example.com/cable'
-  # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
+  config.action_cable.url = 'wss://webrtc-on-rails.herokuapp.com/cable'
+  config.action_cable.allowed_request_origins = [ '*' ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
-  # config.force_ssl = true
+  config.force_ssl = true
 
   # Use the lowest log level to ensure availability of diagnostic information
   # when problems arise.
