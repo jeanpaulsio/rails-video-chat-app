b2dc8ee86a62a55c8dec4b4bc20a46b510b37979
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-10-05 19:17:53 -0700

    refactor integration tests

--- test/integration/rooms_for_guests_test.rb
+++ test/integration/rooms_for_guests_test.rb
@@ -2,24 +2,9 @@ require 'test_helper'
 
 # :nodoc:
 class RoomsForGuestsTest < ActionDispatch::IntegrationTest
-  test 'can see the form to create a room' do
+  def setup
     get root_url
     assert_select 'input#room_name'
-  end
-
-  test 'can create a room as a guest' do
-    get root_url
-
-    assert_difference 'Room.count', 1 do
-      post rooms_path, params: { room: { name: 'room' } }
-    end
-
-    follow_redirect!
-    assert_template 'rooms/show'
-  end
-
-  test 'can claim a room by registering' do
-    get root_url
 
     assert_difference 'Room.count', 1 do
       post rooms_path, params: { room: { name: 'my room' } }
@@ -27,8 +12,27 @@ class RoomsForGuestsTest < ActionDispatch::IntegrationTest
 
     follow_redirect!
     assert_template 'rooms/show'
+    assert_select 'div.alert-success', 'You created the room: my room!'
     assert_select 'h1', 'my room'
+  end
 
+  test 'cannot claim a room' do
+    get claim_room_path
+    follow_redirect!
+    assert_select 'div.alert-notice', 'You must register to claim this room!'
+  end
+
+  test 'cannot change the status of a room' do
+    get toggle_status_room_path(status: :unrestricted)
+    follow_redirect!
+    assert_select 'div.alert-notice', 'You must own this room to do that!'
+
+    get toggle_status_room_path(status: :restricted)
+    follow_redirect!
+    assert_select 'div.alert-notice', 'You must own this room to do that!'
+  end
+
+  test 'can claim a room by registering' do
     sign_up_as(email: 'jerry@rails.com')
     follow_redirect!
     assert_select 'h1', 'my room'
@@ -36,37 +40,5 @@ class RoomsForGuestsTest < ActionDispatch::IntegrationTest
     get claim_room_path
     follow_redirect!
     assert_select 'div.alert-success', 'Room claimed'
-  end
-
-  test 'cannot claim a room without registering' do
-    get root_url
-
-    assert_difference 'Room.count', 1 do
-      post rooms_path, params: { room: { name: 'my room' } }
-    end
-
-    follow_redirect!
-    assert_template 'rooms/show'
-    assert_select 'h1', 'my room'
-
-    get claim_room_path
-    follow_redirect!
-    assert_select 'div.alert-notice', 'You must register to claim this room!'
-  end
-
-  test 'cannot change the status of a room without registering' do
-    get root_url
-
-    assert_difference 'Room.count', 1 do
-      post rooms_path, params: { room: { name: 'my room' } }
-    end
-
-    follow_redirect!
-    assert_template 'rooms/show'
-    assert_select 'h1', 'my room'
-
-    get toggle_status_room_path(status: :unrestricted)
-    follow_redirect!
-    assert_select 'div.alert-notice', 'You must own this room to do that!'
   end
 end
--- test/integration/rooms_for_users_test.rb
+++ test/integration/rooms_for_users_test.rb
@@ -2,31 +2,24 @@ require 'test_helper'
 
 # :nodoc:
 class RoomsForUsersTest < ActionDispatch::IntegrationTest
-  test 'cannot claim an owned rooms' do
-    room  = rooms(:restricted_room)
-    jerry = users(:jerry)
+  def setup
+    kramers_room = rooms(:restricted_room)
+    jerry        = users(:jerry)
 
     log_in_as(jerry)
+    get room_path(kramers_room)
 
-    get room_path(room)
     assert_template 'rooms/show'
     assert_select 'h1', 'Restricted Room'
+  end
 
+  test 'cannot claim someone else\'s room' do
     get claim_room_path
     follow_redirect!
     assert_select 'div.alert-notice', 'Room has been taken'
   end
 
   test 'can only toggle status of owned room' do
-    room  = rooms(:restricted_room)
-    jerry = users(:jerry)
-
-    log_in_as(jerry)
-
-    get room_path(room)
-    assert_template 'rooms/show'
-    assert_select 'h1', 'Restricted Room'
-
     get toggle_status_room_path(status: :unrestricted)
     follow_redirect!
     assert_select 'div.alert-notice', 'You must own this room to do that!'
