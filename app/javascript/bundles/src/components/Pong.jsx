4b45abf31f71ac6b2b721b54ed9326ac58b385b2
Jean-Paul Sio <sio.jeanpaul@gmail.com>
2017-10-12 23:02:16 -0700

    example component making http request with axios

--- app/javascript/bundles/src/components/Pong.jsx
+++ app/javascript/bundles/src/components/Pong.jsx
@@ -1,8 +1,26 @@
 import React, { Component } from "react";
+import axios from "axios";
 
 class Pong extends Component {
+  state = {
+    post: {}
+  }
+
+  componentDidMount() {
+    this.fetchPost()
+  }
+
+  fetchPost() {
+    return (
+      axios
+        .get('https://jsonplaceholder.typicode.com/posts/1')
+        .then(res => this.setState({ post: res.data }))
+        .catch(error => console.warn(error))
+    );
+  }
+
   render() {
-    return <h1>Ping Pong</h1>;
+    return <span />;
   }
 }
 
